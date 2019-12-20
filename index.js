1a16cf5701c620e2293b6132060b17147ccce894
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-20 15:07:32 +0530

    🐛fix ⏱time calculation

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const { windowManager } = require('node-window-manager');
 const path = require('path');
 
 const usageData = {};
+
 let lastTime = Date.now();
 let previousName = path.basename(windowManager.getActiveWindow().path);
 
+usageData[previousName] = 0;
+
 windowManager.addListener('window-activated', window => {
 	const currentTime = Date.now();
+	const currentName = path.basename(window.path);
 
-	if (previousName) {
-		if (usageData.hasOwnProperty(previousName)) {
-			usageData[previousName] += Math.round((currentTime - lastTime) / 1000);
-		} else {
-			usageData[previousName] = 0;
-		}
+	usageData[previousName] += Math.round((currentTime - lastTime) / 1000);
+
+	if (!usageData.hasOwnProperty(currentName)) {
+		usageData[currentName] = 0;
 	}
 
 	previousName = path.basename(window.path);
